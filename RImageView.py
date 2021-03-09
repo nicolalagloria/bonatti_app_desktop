4e76cd7b7a944b99a043191444e8d44ec499a493
Mattia Cantono <mattia@mattias-mbp.lan>
2021-03-08 22:25:54 -0800

    Add Helbronner and Mont Blanc panoramic webcams

--- RImageView.py
+++ RImageView.py
@@ -12,6 +12,8 @@ class LocImage(object):
     location["lavaredo"] = ['https://tl.scenaridigitali.com/trecime/images/image.jpg', 1920, 1080]
     location["pordoi"] = ['https://panodata.panomax.com/cams/1343/canazei_skiarea_belvedere.jpg', 1920, 1080]
     location["cinquetorri"] = ['https://s.skylinewebcams.com/webcam889.jpg', 289, 170]
+    location["helbronner"]=["https://www.lovevda.it/Media/Cache/Webcam/big_courma2.jpg", 1280, 960]
+    location["bianco_pano"]=["https://live-image.panomax.com/cams/1437/recent_reduced.jpg", 3445, 450]
     @classmethod
     def set_name(cls,name):
         cls.name = name
